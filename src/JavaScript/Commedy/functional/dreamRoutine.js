bb1189f29ffd7849c42c05c3b9245ef7c0c20b3d
VitoMelchi <vitomelchionn@gmail.com>
2022-06-30 11:30:53 +0200

    Added dreamRoutine composition

---
+++ src/JavaScript/Commedy/functional/dreamRoutine.js
@@ -0,0 +1,11 @@
+function dreamRoutine(isSummer, currentAge) {
+    while(isSummer && currentAge <= 30){
+        console.log("Eat,");
+        console.log("Sleep,");
+        console.log("Rave,");
+        console.log("Repeat.");
+    }
+}
+
+// Runs the #CodetrySnippet
+dreamRoutine(true, 25);
