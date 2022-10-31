f081ebdd6229e915bb944849feb583fe862e214f
VitoMelchi <vitomelchionn@gmail.com>
2022-10-31 18:45:05 +0100

    Spooky codetry

---
+++ src/JavaScript/Seasonal/functional/Halloween.js
@@ -0,0 +1,12 @@
+/**
+ * ^Hapyp  hAlLoweeN~ >:D
+ ******/
+function bOO(isSPOOKY_Sesaon, evillauhg) {
+  if (isSPOOKY_Sesaon)
+    while (isSPOOKY_Sesaon) {
+      evillauhg.concat(evillauhg);
+      console.log(evillauhg);
+    }
+}
+
+bOO(true, "MuaHaahahahaAHAHAHA");
